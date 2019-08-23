a5bf67f6d5686f84914365c605ac9abb81459f17
jessicakwalters <jessicakwalters@gmail.com>
2019-08-22 20:49:34 -0700

    generic routes

---
+++ src/routes/api.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const router = express.Router();
+
+const modelLoader = require('../middleware/model-loader');
+
+router.param('model', modelLoader);
+
+/**
+ * This function comment is parsed by doctrine
+ * @route GET /api/v1/:model
+ * @param {string} model name required
+ * @returns {object} 200
+ * @returns {Error}  default - Unexpected error
+ */
+router.get('/api/v1/:model', handleGetAll);
+/**
+ * Create an object
+ * @route POST /api/v1/:model
+ * @param {string} model name required
+ * @body  {object}
+ * @returns {object} 200
+ * @returns {Error}  default - Unexpected error
+ */
+router.post('/api/v1/:model', handleCreate);
+/**
+ * Get an object
+ * @route GET /api/v1/:model/:id
+ * @param {string} model name required
+ * @param {id} item id required
+ * @returns {object} 200
+ * @returns {Error}  default - Unexpected error
+ */
+router.get('/api/v1/:model/:id', handleGetOne);
+/**
+ * Get an object
+ * @route PUT /api/v1/:model/:id
+ * @param {string} model name required
+ * @param {id} item id required
+ * @body  {object}
+ * @returns {object} 200
+ * @returns {Error}  default - Unexpected error
+ */
+router.put('/api/v1/:model/:id', handleUpdate);
+/**
+ * Get an object
+ * @route DELETE /api/v1/:model/:id
+ * @param {string} model name required
+ * @param {id} item id required
+ * @returns {object} 200
+ * @returns {Error}  default - Unexpected error
+ */
+router.delete('/api/v1/:model/:id', handleDelete);
+
+// ROUTE HANDLER FUNCTIONS
+
+/**
+ * Retrieves one category
+ * @param {request}
+ * @param {response}
+ * @param {next}
+ * @returns
+ */
+function handleGetAll(request,response,next) {
+    // expects an array of object to be returned from the model
+    request.model.get()
+        .then( data => {
+            const output = {
+                count: data.length,
+                results: data,
+            };
+            response.status(200).json(output);
+        })
+        .catch( next );
+}
+
+/**
+ * Retrieves all the categories in the database
+ * @param {request}
+ * @param {response}
+ * @param {next}
+ * @returns
+ */
+function handleGetOne(request,response,next) {
+    // expects an array with the one matching record from the model
+    request.model.get(request.params.id)
+        .then( result => response.status(200).json(result) )
+        .catch( next );
+}
+
+/**
+ * Creates a category
+ * @param {request}
+ * @param {response}
+ * @param {next}
+ * @returns
+ */
+function handleCreate(request,response,next) {
+    // expects the record that was just added to the database
+    request.model.create(request.body)
+        .then( result => {
+            response.status(200).json(result);
+        } )
+        .catch( next );
+}
+
+/**
+ * Updates a category
+ * @param {request}
+ * @param {response}
+ * @param {next}
+ * @returns
+ */
+function handleUpdate(request,response,next) {
+    // expects the record that was just updated in the database
+    request.model.update(request.params.id, request.body)
+        .then( result => response.status(200).json(result) )
+        .catch( next );
+}
+
+/**
+ * Deletes a category
+ * @param {request}
+ * @param {response}
+ * @param {next}
+ * @returns
+ */
+function handleDelete(request,response,next) {
+    // Expects no return value (resource was deleted)
+    request.model.delete(request.params.id)
+        .then( result => response.status(200).json(result) )
+        .catch( next );
+}
+
+module.exports = router;
